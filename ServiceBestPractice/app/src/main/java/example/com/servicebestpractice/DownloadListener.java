9c70973b086a053e5c44d8ddf263e44084dd8f0c
minling910 <2537985277@qq.com>
2020-01-05 19:47:44 +0800

    no message

---
+++ ServiceBestPractice/app/src/main/java/example/com/servicebestpractice/DownloadListener.java
@@ -0,0 +1,10 @@
+package example.com.servicebestpractice;
+
+
+public interface DownloadListener {
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
